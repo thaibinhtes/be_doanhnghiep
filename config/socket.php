a901135ba71212119b43ec6d98578ea209439f75
thaibinhpressify <thaibinhpressify@gmail.com>
2026-07-05 11:26:50 +0700

    update connect socket

--- config/socket.php
+++ config/socket.php
@@ -3,7 +3,7 @@
 return [
     'redis_channel' => env('SOCKET_REDIS_CHANNEL', 'mobi:import-events'),
 
-    'internal_url' => env('SOCKET_INTERNAL_URL', 'http://127.0.0.1:6001'),
+    'internal_url' => env('SOCKET_INTERNAL_URL', 'http://127.0.0.1:3001'),
 
     'internal_secret' => env('SOCKET_INTERNAL_SECRET', 'mobi-socket-internal'),
 ];
